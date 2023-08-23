d5231d1064bfb7cf1c4796ac2a48a563512947ef
nikitha maram <nikitha.maram@runo.in>
2023-08-23 12:43:51 +0530

    Upgraded the changes to v2.7

--- lib/main.dart
+++ lib/main.dart
@@ -94,7 +94,7 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Hello, Flutter Web!'),
+        title: Text('Hello, Flutter Web 2.7!'),
       ),
       body: Center(
         child: Column(

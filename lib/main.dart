1e9301f073ffd5a266ea294d1a539022fb289496
nikitha maram <nikitha.maram@runo.in>
2023-08-23 12:12:52 +0530

    Upgraded the changes to v2.4

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,73 @@
+// import 'package:flutter/material.dart';
+// import 'package:package_info_plus/package_info_plus.dart';
+// void main() {
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Cache and Service Worker Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: MyHomePage(),
+//     );
+//   }
+// }
+//
+// class MyHomePage extends StatefulWidget {
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//   PackageInfo _packageInfo = PackageInfo(
+//     appName: 'Unknown',
+//     packageName: 'Unknown',
+//     version: 'Unknown',
+//     buildNumber: 'Unknown',
+//     buildSignature: 'Unknown',
+//     installerStore: 'Unknown',
+//   );
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     _initPackageInfo();
+//   }
+//
+//   Future<void> _initPackageInfo() async {
+//     final info = await PackageInfo.fromPlatform();
+//     setState(() {
+//       _packageInfo = info;
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//
+//     print(_packageInfo.version);
+//     return Scaffold(
+//       // appBar: AppBar(
+//       //   title: Text('Cache and Service Worker Demo'),
+//       // ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//
+//
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
-import 'package:package_info_plus/package_info_plus.dart';
+import 'dart:html' as html;
+
 void main() {
   runApp(MyApp());
 }
@@ -8,7 +76,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Cache and Service Worker Demo',
+      title: 'Simple Flutter Web App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -17,55 +85,30 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  PackageInfo _packageInfo = PackageInfo(
-    appName: 'Unknown',
-    packageName: 'Unknown',
-    version: 'Unknown',
-    buildNumber: 'Unknown',
-    buildSignature: 'Unknown',
-    installerStore: 'Unknown',
-  );
-
-  @override
-  void initState() {
-    super.initState();
-    _initPackageInfo();
-  }
-
-  Future<void> _initPackageInfo() async {
-    final info = await PackageInfo.fromPlatform();
-    setState(() {
-      _packageInfo = info;
-    });
+class MyHomePage extends StatelessWidget {
+  void _openLink() {
+    html.window.open('https://flutter.dev', 'Flutter Website');
   }
 
   @override
   Widget build(BuildContext context) {
-
-    print(_packageInfo.version);
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text('Cache and Service Worker Demo'),
-      // ),
+      appBar: AppBar(
+        title: Text('Hello, Flutter Web!'),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              'Version: 2.2.0', // Change version number
+              'Hello, Flutter Web!',
               style: TextStyle(fontSize: 24),
             ),
-            Text(
-              'Welcome to the updated app! New version !!! ', // Change greeting message
-              style: TextStyle(fontSize: 18,color: Colors.black),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _openLink,
+              child: Text('Visit Flutter Website'),
             ),
-
           ],
         ),
       ),

3df48e2fd2e20391e563eb0d87bede76875d6d4c
nikitha maram <nikitha.maram@runo.in>
2023-08-23 15:03:38 +0530

    Minor changes 3.0

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,162 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: HomePage1(),
     );
   }
 }
 
+class HomePage1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome to the Home Page',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => AboutPage()));
+              },
+              child: Text('Go to About Page'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage2()));
+              },
+              child: Text('Go to Contact Page'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AboutPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('About Page'),
+      ),
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              'This is the About Page.',
+              style: TextStyle(fontSize: 24),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
+              },
+              child: Text('Go to About Page'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ContactPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contact Page'),
+      ),
+      backgroundColor: Colors.red,
+      body: Center(
+        child: Text(
+          'This is the Contact Page.',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
 class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome to the Home Page',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage1()));
+              },
+              child: Text('Go to About Page'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => ContactPage()));
+              },
+              child: Text('Go to Contact Page'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome to the Home Page',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => AboutPage()));
+              },
+              child: Text('Go to About Page'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage3()));
+              },
+              child: Text('Go to Contact Page'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,42 +196,6 @@ class HomePage extends StatelessWidget {
               child: Text('Go to Contact Page'),
             ),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class AboutPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('About Page'),
-      ),
-      backgroundColor: Colors.blue,
-      body: Center(
-        child: Text(
-          'This is the About Page.',
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
-    );
-  }
-}
-
-class ContactPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Contact Page'),
-      ),
-      backgroundColor: Colors.red,
-      body: Center(
-        child: Text(
-          'This is the Contact Page.',
-          style: TextStyle(fontSize: 24),
         ),
       ),
     );

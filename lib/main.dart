e28009d15b90bce65232940b82319d2d0976c975
nikitha maram <nikitha.maram@runo.in>
2023-08-23 14:34:06 +0530

    Minor changes 3.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,4 @@
-// import 'package:flutter/material.dart';
-// import 'package:package_info_plus/package_info_plus.dart';
-// void main() {
-//   runApp(MyApp());
-// }
-//
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Cache and Service Worker Demo',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: MyHomePage(),
-//     );
-//   }
-// }
-//
-// class MyHomePage extends StatefulWidget {
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-//
-// class _MyHomePageState extends State<MyHomePage> {
-//   PackageInfo _packageInfo = PackageInfo(
-//     appName: 'Unknown',
-//     packageName: 'Unknown',
-//     version: 'Unknown',
-//     buildNumber: 'Unknown',
-//     buildSignature: 'Unknown',
-//     installerStore: 'Unknown',
-//   );
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     _initPackageInfo();
-//   }
-//
-//   Future<void> _initPackageInfo() async {
-//     final info = await PackageInfo.fromPlatform();
-//     setState(() {
-//       _packageInfo = info;
-//     });
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//
-//     print(_packageInfo.version);
-//     return Scaffold(
-//       // appBar: AppBar(
-//       //   title: Text('Cache and Service Worker Demo'),
-//       // ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//
-//
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
-import 'dart:html' as html;
 
 void main() {
   runApp(MyApp());
@@ -76,41 +8,78 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Simple Flutter Web App',
+      title: 'Simple Multi-Page App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  void _openLink() {
-    html.window.open('https://flutter.dev', 'Flutter Website');
-  }
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Hello, Flutter Web 3.0!'),
+        title: Text('Home Page'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              'Hello, Flutter Web!',
+              'Welcome to the Home Page',
               style: TextStyle(fontSize: 24),
             ),
             SizedBox(height: 20),
-            Image.asset('assets/download.jpeg'),
             ElevatedButton(
-              onPressed: _openLink,
-              child: Text('Visit Flutter Website'),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => AboutPage()));
+              },
+              child: Text('Go to About Page'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => ContactPage()));
+              },
+              child: Text('Go to Contact Page'),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class AboutPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('About Page'),
+      ),
+      body: Center(
+        child: Text(
+          'This is the About Page.',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
+
+class ContactPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contact Page'),
+      ),
+      body: Center(
+        child: Text(
+          'This is the Contact Page.',
+          style: TextStyle(fontSize: 24),
         ),
       ),
     );

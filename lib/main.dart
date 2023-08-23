99fff89afe5364d9edeafedf765574f69505338e
nikitha maram <nikitha.maram@runo.in>
2023-08-23 16:53:19 +0530

    Minor changes 3.0

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class HomePage1 extends StatelessWidget {
           children: [
             Text(
               'Welcome to the Home Page',
-              style: TextStyle(fontSize: 24,color: Colors.pink),
+              style: TextStyle(fontSize: 24,color: Colors.yellow),
             ),
             SizedBox(height: 20),
             ElevatedButton(

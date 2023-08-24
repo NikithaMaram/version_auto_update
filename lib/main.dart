6a4c709f147060d56313c85b6c371db9a90ba1df
nikitha maram <nikitha.maram@runo.in>
2023-08-24 16:43:37 +0530

    Minor changes 3.0

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class HomePage1 extends StatelessWidget {
       appBar: AppBar(
         title: Text('Home Page 1'),
       ),
-      backgroundColor: Colors.green,
+      backgroundColor: Colors.black,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
